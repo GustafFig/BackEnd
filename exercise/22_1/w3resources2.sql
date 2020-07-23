c08f2077ac2110144473325cf151f995250a3836
Gustaff <gustavohmfigueiredo@gmail.com>
2020-07-23 18:26:24 -0300

    until 2

---
+++ exercise/22_1/w3resources2.sql
@@ -0,0 +1,13 @@
+-- EXERCISE 22_1
+-- https://www.w3resource.com/mysql-exercises/create-table-exercises/
+
+-- 2. Write a SQL statement to create a simple table countries including columns
+-- country_id,country_name and region_id which is already exists.
+
+CREATE TABLE IF NOT EXISTS countries(
+	country_id CHAR(2),
+    country_name VARCHAR(50),
+    region_id INT
+);
+
+DESC ex22day1.countries;
---
+++ exercise/22_1/w3resources3.sql
@@ -0,0 +1,4 @@
+-- EXERCISE 22_1
+-- https://www.w3resource.com/mysql-exercises/create-table-exercises/
+
+-- 3. Write a SQL statement to create the structure of a table dup_countries similar to countries.
