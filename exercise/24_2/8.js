d1e1f69bb9ddcb0ad847aef23fe76bc4326b5c10
Gustaff <gustavohmfigueiredo@gmail.com>
2020-08-04 18:43:38 -0300

    8.js insert an array of documents

--- exercise/24_2/8.js
+++ exercise/24_2/8.js
@@ -1,1 +1,36 @@
 // Exercício 8: Crie um array de documentos chamado cast para o filme Home Alone com os seguintes dados:
+
+/*
+{
+  "actor": "Macaulay Culkin",
+  "character": "Kevin"
+},
+{
+  "actor": "Joe Pesci",
+  "character": "Harry"
+},
+{
+  "actor": "Daniel Stern"
+}
+*/
+
+db.movies.updateOne(
+  { "title": "Home Alone" },
+  {
+    $push: {
+      "cast": [
+        {
+          "actor": "Macaulay Culkin",
+          "character": "Kevin"
+        },
+        {
+          "actor": "Joe Pesci",
+          "character": "Harry"
+        },
+        {
+          "actor": "Daniel Stern"
+        }
+      ]
+    }
+  }
+);
