dae5d57a61c36df0101f849b23033d3b2c087be8
Gustaff <gustavohmfigueiredo@gmail.com>
2020-08-04 19:33:02 -0300

    12.js with '$'

--- exercise/24_2/11.js
+++ exercise/24_2/11.js
@@ -3,3 +3,16 @@
 // Adicione o campo actor com o valor Christian Bale ao array de cast em que o campo actor seja igual a Batman;
 // Adicione o campo actor com o valor Michael Caine ao array de cast em que o campo actor seja igual a Alfred;
 // Adicione o campo actor com o valor Heath Ledger ao array de cast em que o campo actor seja igual a Coringa;
+
+db.movies.updateOne(
+  { "title": "Batman", "cast.character": "Batman" },
+  { $set: { "cast.$.actor": "Christian Bale" } }
+);
+db.movies.updateOne(
+  { "title": "Batman", "cast.character": "Alfred" },
+  { $set: { "cast.$.actor": "Michael Caine" } }
+);
+db.movies.updateOne(
+  { "title": "Batman", "cast.character": "Coringa" },
+  { $set: { "cast.$.actor": "Heath Ledger" } }
+);
