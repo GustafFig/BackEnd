98a7bf86c2d5b856e813dc03f77e80bf03c8b95b
Gustaff <gustavohmfigueiredo@gmail.com>
2020-08-04 18:50:38 -0300

    8.js correcting with each

--- exercise/24_2/8.js
+++ exercise/24_2/8.js
@@ -18,19 +18,21 @@ db.movies.updateOne(
   { "title": "Home Alone" },
   {
     $push: {
-      "cast": [
-        {
-          "actor": "Macaulay Culkin",
-          "character": "Kevin"
-        },
-        {
-          "actor": "Joe Pesci",
-          "character": "Harry"
-        },
-        {
-          "actor": "Daniel Stern"
-        }
-      ]
+      "cast": {
+        $each: [
+          {
+            "actor": "Macaulay Culkin",
+            "character": "Kevin"
+          },
+          {
+            "actor": "Joe Pesci",
+            "character": "Harry"
+          },
+          {
+            "actor": "Daniel Stern"
+          }
+        ]
+      }
     }
   }
 );
