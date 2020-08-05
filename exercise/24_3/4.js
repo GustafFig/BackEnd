051fe6d8459df954ed531879222a778a90a6899c
Gustaff <gustavohmfigueiredo@gmail.com>
2020-08-05 18:16:20 -0300

    4.js

--- exercise/24_3/4.js
+++ exercise/24_3/4.js
@@ -1,1 +1,10 @@
 // Adicione um array chamado ratings ao filme Godzilla com os seguintes valores: [78, 52, 95, 102].
+
+db.movies.update(
+  { title: "Godzilla" },
+  {
+    $push: {
+      ratings: { $each: [78, 52, 95, 102] }
+    }
+  }
+);
