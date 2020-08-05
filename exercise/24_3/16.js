9d5b1675fedb10b99e384e2c53e62902d7a51ab5
Gustaff <gustavohmfigueiredo@gmail.com>
2020-08-05 18:50:13 -0300

    16.js

--- exercise/24_3/16.js
+++ exercise/24_3/16.js
@@ -1,1 +1,6 @@
 // Adicione o campo description no filme Godzilla com o seguinte valor: "The world is beset by the appearance of monstrous creatures, but one of them may be the only one who can save humanity.".
+
+db.movies.update(
+  { title: "Godzilla" },
+  { $set: { description: "The world is beset by the appearance of monstrous creatures, but one of them may be the only one who can save humanity." } }
+);
