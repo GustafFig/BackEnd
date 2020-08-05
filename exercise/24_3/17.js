f88ba181fe77ac5852bccc39f3bc232204cbe30f
Gustaff <gustavohmfigueiredo@gmail.com>
2020-08-05 18:51:51 -0300

    17.js

--- exercise/24_3/17.js
+++ exercise/24_3/17.js
@@ -1,1 +1,6 @@
 // Adicione o campo description no filme Home Alone com o seguinte valor: "An eight-year-old troublemaker must protect his house from a pair of burglars when he is accidentally left home alone by his family during Christmas vacation.".
+
+db.movies.update(
+  { title: "Home Alone" },
+  { $set: { description: "An eight-year-old troublemaker must protect his house from a pair of burglars when he is accidentally left home alone by his family during Christmas vacation." } }
+);
