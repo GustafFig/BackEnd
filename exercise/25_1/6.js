2c066a2033c33857641b851c15ff78b1a02decd5
Gustaff <gustavohmfigueiredo@gmail.com>
2020-08-07 17:54:46 -0300

    6.js

--- exercise/25_1/6.js
+++ exercise/25_1/6.js
@@ -1,1 +1,8 @@
 // Agrupe os clientes por sexo e uf. Retorne o total de clientes de cada sexo no campo total.
+
+db.clientes.aggregate([
+  { $group: {
+    _id: { sexo: "$sexo", UF: "$endereco.uf" },
+    total: { $sum: 1 }
+  }}
+]);
