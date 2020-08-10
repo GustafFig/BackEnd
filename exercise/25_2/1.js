64070ddcb39c02159ca29c5a6e227aee9b95d1b4
Gustaff <gustavohmfigueiredo@gmail.com>
2020-08-10 19:02:22 -0300

    2.js and constante miliseconds

--- exercise/25_2/1.js
+++ exercise/25_2/1.js
@@ -4,12 +4,14 @@
 // calcule a idade usando a diferença entre a data corrente e a data de nascimento;
 // 1 dia é igual a 86400000 milissegundos.
 
+const milissegundos = 365 * 86400000
+
 db.clientes.aggregate([
   { $addFields: {
     idade: { $floor: { 
       $divide: [
         { $subtract: ["$$NOW", "$dataNascimento"] }
-        , 31536000000
+        , milissegundos
       ]
     } }
   } },
