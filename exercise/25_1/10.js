4f0e8c1fdc9f4be339dc37369badaab8c6688e71
Gustaff <gustavohmfigueiredo@gmail.com>
2020-08-10 15:13:00 -0300

    10.js

--- exercise/25_1/10.js
+++ exercise/25_1/10.js
@@ -1,2 +1,13 @@
 // Descubra quantos clientes compraram mais de 5 vezes. Retorne um documento que contenha somente o campo clientes com o o total de clientes.
 // Dica: O operador $count pode simplificar sua query.
+
+db.vendas.aggregate([
+  { $group: {
+    _id: "$clienteId",
+    comprasQty: { $sum: 1 }
+  } },
+  { $match: {
+    comprasQty: { $gt: 5 }
+  } },
+  { $count: "clientes" }
+]);
