50c65d5fa80f2b4c2b7ab0f1012aa4422f119796
Gustaff <gustavohmfigueiredo@gmail.com>
2020-08-10 19:29:13 -0300

    4.js

---
+++ exercise/25_2/4.js
@@ -0,0 +1,26 @@
+// Exercício 4: Selecione TODOS os clientes que compraram entre Junho de 2019 e Março de 2020.
+
+db.clientes.aggregate([
+  { $addFields: {
+    idade: { $floor: { 
+      $divide: [
+        { $subtract: ["$$NOW", "$dataNascimento"] },
+        milissegundos
+      ]
+    } }
+  } },
+  { $match: {
+    idade: { $gte: 18, $lt: 25 }
+  } },
+  { $lookup: {
+    from: "vendas",
+    localField: "clienteId",
+    foreignField: "clienteId",
+    as: "compras"
+  } },
+  { $match: {
+    "compras.dataVenda": { $gte: ISODate("2019-06-01"), $lte: ISODate("2019-03-31") }
+  } },
+  { $project: { nome: 1 }},
+  { $count: "qty"}
+]);
