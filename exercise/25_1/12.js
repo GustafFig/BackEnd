821cb48ff26fd31d30657fbc264deee59de6e53c
Gustaff <gustavohmfigueiredo@gmail.com>
2020-08-10 15:52:40 -0300

    12.js

--- exercise/25_1/12.js
+++ exercise/25_1/12.js
@@ -1,1 +1,29 @@
 // Descubra qual as três ufs que mais compraram no ano de 2020. Retorne os documentos no seguinte formato:
+
+/*
+{
+  "totalVendas": 10,
+  "uf": "SP"
+}
+*/
+
+db.vendas.aggregate([
+  { $lookup: {
+    from: "clientes",
+    localField: "clienteId",
+    foreignField: "clienteId",
+    as: "cliente"
+  } },
+  { $unwind: "$cliente" },
+  { $group: {
+    _id: "$cliente.endereco.uf",
+    totalVendas: { $sum: 1 }
+  } },
+  { $sort: { totalVendas: -1 } },
+  { $limit: 3 },
+  { $project: {
+    _id: 0,
+    uf: "$_id",
+    totalVendas: 1
+  } }
+]).pretty();
