25272a8694852bd59938ee50979d15e9f66b4b35
Gustaff <gustavohmfigueiredo@gmail.com>
2020-08-14 20:05:33 -0300

    1.js

--- exercise/26_2/1.js
+++ exercise/26_2/1.js
@@ -1,20 +1,24 @@
 const inquirer = require('inquirer');
 
-const promise = () => new Promise( async (resolve, reject)  => {
-  const { a, b, c } = await inquirer.prompt([
+const promise = () => new Promise((resolve, reject)  => {
+  inquirer.prompt([
     { type: 'number', name: 'a', message: 'a: '},
     { type: 'number', name: 'b', message: 'b: '},
     { type: 'number', name: 'c', message: 'c: '},
-  ]);
+  ]).then(({ a, b, c }) => {
+    if (typeof a !== 'number' || Number.isNaN(a)) return reject("Digite apenas números");
+    if (typeof b !== 'number' || Number.isNaN(b)) return reject("Digite apenas números");
+    if (typeof c !== 'number' || Number.isNaN(c)) return reject("Digite apenas números");
 
-  if (typeof a !== 'number') return reject("Digite apenas números");
-  if (typeof b !== 'number') return reject("Digite apenas números");
-  if (typeof c !== 'number') return reject("Digite apenas números");
+    const value = (a + b) * c;
 
-  const value = (a + b) * c;
-  
-  if (value < 50) return reject("Valor muito baixo");
-  resolve(value);
+    if (value < 50) return reject("Valor muito baixo");
+    resolve(`Seu valor é ${value}`);
+  }).catch(() => {
+    if (typeof a !== 'number') return reject("Digite apenas números");
+    if (typeof b !== 'number') return reject("Digite apenas números");
+    if (typeof c !== 'number') return reject("Digite apenas números");
+  });
 });
 
 promise()
