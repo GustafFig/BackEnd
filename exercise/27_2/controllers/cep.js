09595554afa318710b1dc77edb7595156c3db99b
Gustaff <gustavohmfigueiredo@gmail.com>
2020-08-22 22:54:58 -0300

    race

--- exercise/27_2/controllers/cep.js
+++ exercise/27_2/controllers/cep.js
@@ -1,16 +1,15 @@
-const { lookupCEP, fromOutside, storeCep } = require('../models/cep');
+const { searchFirst, fromOutside, storeCep } = require('../models/cep');
 
 async function searchInformation(req, res, next) {
   try {
     const { cep } = req.body;
-    const result = await lookupCEP(cep);
-    if (result.length !== 0) {
-      return res.render('cepResponse', { ceps: result });
-    }
-    
-    const outsideResult = await fromOutside(cep);
-    await storeCep(outsideResult);
-    res.render('cepResponse', { ceps: outsideResult });
+
+    const ceps = await Promise.race([
+      searchFirst(cep),
+      fromOutside(cep),
+    ]);
+
+    res.render('cepResponse', { ceps });
   } catch (err) {
     console.error(err);
     next('error ao procurar informação');
--- exercise/27_2/models/cep.js
+++ exercise/27_2/models/cep.js
@@ -3,21 +3,35 @@ const fetch = require('node-fetch');
 
 const CEP_PATTERN = ['cep', 'uf', 'cidade', 'bairro', 'logradouro'];
 
-async function storeCep(ceps) {
+async function storeOneCep(cepInst, table) {
   try {
-    console.info('before insert', ceps);
-    const db = await connection();
-    const table = await db.getTable('cep');
-    const status = ceps.forEach((cepInst) => {
-      const { cep, uf, cidade, bairro, logradouro } = cepInst;
-      table
+    const { cep, uf, cidade, bairro, logradouro } = cepInst;
+    const existisCep = await lookupCEP(cep).length;
+    console.log(existisCep)
+    if (existisCep) return;
+    await table
       .insert(['id', 'uf', 'cidade', 'bairro', 'logradouro'])
       .values(cep, uf, cidade, bairro, logradouro)
       .execute();
-    });
-    console.info('after insert', status);
+    console.log('saved')
+
   } catch (err) {
-    console.err(err);
+    console.error('inside storeOneCep', err);
+  }
+}
+
+async function storeCep(ceps) {
+  try {
+    const db = await connection();
+    const table = db.getTable('cep');
+
+    for (let i = 0; i < ceps.length; i += 1) {
+      await storeOneCep(ceps[i], table);
+    }
+
+    return ceps;
+  } catch (err) {
+    console.error(err);
   }
 }
 
@@ -31,7 +45,7 @@ async function connection() {
     });
     return session.getSchema('cep_lookup');  
   } catch (err) {
-    console.error(err);
+    console.error('inside connection', err);
     process.exit(1);
   }
 }
@@ -45,17 +59,18 @@ function transformToObj(arr) {
 
 async function lookupCEP(id) {
   try {
-    const db = await connection();
-    const table = await db.getTable('cep');
+    const db = await connection()
+    const table = db.getTable('cep');
+
     const cep = await table
       .select(['id', 'uf', 'cidade', 'bairro', 'logradouro'])
-      .where('id', ':id')
+      .where('id = :id')
       .bind('id', id)
       .execute();
-    const results = await cep.fetchAll();
-    return results.map(transformToObj);
+
+    return cep.fetchAll();
   } catch (err) {
-    console.error(err);
+    console.error('inside lookup', err);
   }
 }
 
@@ -65,16 +80,34 @@ async function fromOutside(cep) {
       method: 'GET',
       headers: { Accept: 'application/json' }
     });
+
     console.log('status', response.url);
-    const json = await response.json();
-    return json;
+    const ceps = await response.json();
+
+    await storeCep(ceps);
+
+    return ceps;
   } catch (err) {
-    console.error(err);
+    console.error('inside from Outside', err);
+  }
+}
+
+async function searchFirst(cep) {
+  try {
+    const results = await lookupCEP(cep);
+
+    if (results.length !== 0) {
+      return results.map(transformToObj);
+    }
+
+    return fromOutside(cep);
+  } catch (err) {
+    console.error('inside searchFirst', err);
   }
 }
 
 module.exports = {
-  lookupCEP,
+  searchFirst,
   fromOutside,
   storeCep,
 };
