a418e37656cf0d9eee0bf9422f992cbbd477ec91
Gustaff <gustavohmfigueiredo@gmail.com>
2020-09-09 19:38:19 -0300

    reading photo

--- exercise/29_2/controllers/index.js
+++ exercise/29_2/controllers/index.js
@@ -1,5 +1,7 @@
 const text = require('./text');
+const img = require('./img');
 
 module.exports = {
   text,
+  img,
 };
--- exercise/29_2/index.js
+++ exercise/29_2/index.js
@@ -1,14 +1,16 @@
 const express = require('express');
-const { text } = require('./controllers');
+const { text, img } = require('./controllers');
 
 const app = express();
 
 const { PORT = 3000 } = process.env;
 
 app.use('/text', text);
+app.use('/img', img);
 
 app.use((err, _req, res, _next) => {
   res.json({ errorMessage: err.message });
 });
+
 
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`));
