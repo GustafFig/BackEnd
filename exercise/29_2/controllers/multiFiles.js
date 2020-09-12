8e4439b65ffe9be7b6b7c409f48da76934ff2aa6
Gustaff <gustavohmfigueiredo@gmail.com>
2020-09-12 20:10:39 -0300

    obrigatorios feitos

--- exercise/29_2/controllers/multiFiles.js
+++ exercise/29_2/controllers/multiFiles.js
@@ -7,9 +7,8 @@ const multi = express.Router();
 
 const storage = multer.diskStorage({
   destination: path.join(__dirname, 'multi'),
-  filename: (_req, file, callback) => {
-    console.log('file', file)
-    callback(null, file.originalname);
+  filename: (_req, { originalname }, callback) => {
+    callback(null, originalname);
   },
 });
 
@@ -17,9 +16,11 @@ const uploadMany = multer({ storage });
 
 multi.post('/', uploadMany.array('files', 10), (req, res, next) => {
   const { files } = req;
-  console.log(Object.keys(req));
+
   if (!files) return next({ message: 'Any file' });
+
   const names =  files.map(({ originalname }) => originalname );
+
   return res.status(201).json({ files: names });
 });
 
